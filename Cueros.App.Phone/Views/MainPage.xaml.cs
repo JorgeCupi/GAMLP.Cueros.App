6d6ef580a74fee07bf4b43f5549806f8ccfb199e
PC-Yolger <yolger_pc522@hotmail.com>
2013-11-12 15:29:58 -0400

    nuevo

--- Cueros.App.Phone/Views/MainPage.xaml.cs
+++ Cueros.App.Phone/Views/MainPage.xaml.cs
@@ -22,6 +22,11 @@ namespace Cueros.App.Phone
             //BuildLocalizedApplicationBar();
         }
 
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
         // Sample code for building a localized ApplicationBar
         //private void BuildLocalizedApplicationBar()
         //{
