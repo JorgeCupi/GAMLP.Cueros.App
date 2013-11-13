e42c0a46866e27dc3020bfb4ab7f7f20837d1e74
PC-Yolger <yolger_pc522@hotmail.com>
2013-11-13 19:59:42 -0400

    consumo

--- Cueros.App.Phone/ViewModels/CategoriaViewModel.cs
+++ Cueros.App.Phone/ViewModels/CategoriaViewModel.cs
@@ -6,20 +6,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using Cueros.App.Core.Models;
+using Cueros.App.Core.Services;
 
 namespace Cueros.App.Phone.ViewModels
 {
     public class CategoriaViewModel : NotificationEnabledObject
     {
-        private ObservableCollection<Models.Categoria> categoria;
-        public ObservableCollection<Models.Categoria> Categoria
+        private ObservableCollection<Producto> productos;
+        public ObservableCollection<Producto> Productos
         {
-            get { return categoria; }
-            set { categoria = value; }
+            get { return productos; }
+            set { productos = value; }
         }
 
         public CategoriaViewModel()
         {
+            datos();
+        }
+
+        public async void datos()
+        {
+            List<Producto> pro = await ServiciosDeProductos.ObtenerProductos();
+            productos = new ObservableCollection<Producto>(pro);
         }
     }
 }
