5a74ebac3797efe3bee8df34c2568cc2908cee38
Jorge Cupi <jorgecupi@hotmail.com>
2013-11-16 20:03:42 -0400

    Creacion de clase categoria
    Adicion de nuevos atributos

---
+++ Cueros.App.Core/Models/Categoria.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cueros.App.Core.Models
+{
+    public class Categoria
+    {
+        public string Linea { get; set; }
+
+        public string UrlImagen { get; set; }
+    }
+}
