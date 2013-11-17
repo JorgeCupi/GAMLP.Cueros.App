3bc1a0426b20214510b57fd0b4d0cf60c8709b9a
Jorge Cupi <jorgecupi@hotmail.com>
2013-11-16 21:12:46 -0400

    DONE: Metodo para obtener lista de categorias
    Ya deberia funcionar perfectamente esto, denle una revisada

--- Cueros.App.Core/Services/ServiciosDeCategorias.cs
+++ Cueros.App.Core/Services/ServiciosDeCategorias.cs
@@ -11,7 +11,7 @@ namespace Cueros.App.Core.Services
     {
         public static async Task<List<Categoria>> ObtenerListaDeCategorias()
         {
-            string url = "https://dl.dropboxusercontent.com/s/7f402dzmqmw871m/CUEROS.json?dl=1&token_hash=AAHNw7NngxtL3TRef5pTNFNe-jYRKekuEczDA8bhEnvdsQ";
+            string url = "https://dl.dropboxusercontent.com/s/6ysajpdq04qkcxv/Categorias.json?dl=1&token_hash=AAEesyUbBh7vEzrybQ_5Ik7DtEDgTWOmCe2wT7Cv61rTEQ";
             string response = await Utilities.DownloadJsonFromThisUrl(url);
 
             return Utilities.TransformToCategoriesList(response);
