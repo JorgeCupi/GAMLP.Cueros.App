e47a2f3e2865be106aac62ef7dab7c5ca4bcdd6a
QHebert <hebertfqc81@hotmail.com>
2013-11-24 02:18:55 -0400

    carrito

---
+++ Cueros.App.WPF/Views/Carrito.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Cueros.App.Core.Models;
+
+namespace Cueros.App.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for Carrito.xaml
+    /// </summary>
+    public partial class Carrito : Page
+    {
+        List<Producto> Pro = new List<Producto>();
+        public Carrito(List<Producto> p)
+        {
+            Pro = p;
+            InitializeComponent();
+        }
+
+    }
+}
