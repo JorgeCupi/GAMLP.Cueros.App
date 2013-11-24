035295441e95b38b19583c8abf2ddc35668c4fc7
QHebert <hebertfqc81@hotmail.com>
2013-11-23 20:20:59 -0400

    Add Inicio

---
+++ Cueros.App.WPF/Views/Inicio.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cueros.App.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for Inicio.xaml
+    /// </summary>
+    public partial class Inicio : Window
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+        private void Click_Cueros(object sender, RoutedEventArgs e)
+        {
+            MainWindow MainW = new MainWindow();
+            MainW.Show();
+            this.Hide();
+        }
+        private void Click_work(object sender, RoutedEventArgs e)
+        {
+            Working MainW = new Working();
+            MainW.Show();
+            this.Hide();
+        }
+    }
+}
---
+++ Cueros.App.WPF/Views/Working.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Cueros.App.WPF.Views
+{
+    /// <summary>
+    /// Interaction logic for Working.xaml
+    /// </summary>
+    public partial class Working : Window
+    {
+        public Working()
+        {
+            InitializeComponent();
+        }
+        private void Click_Volver(object sender, RoutedEventArgs e)
+        {
+            Inicio MainW = new Inicio();
+            MainW.Show();
+            this.Hide();
+        }
+    }
+}
