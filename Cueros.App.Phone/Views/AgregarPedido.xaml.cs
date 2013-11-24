6665c52c1324330987487850f24690563221cbd2
AlisonParisaca <aly_pink7@hotmail.com>
2013-11-24 00:03:30 -0300

    :D Subiendo Cambios

--- Cueros.App.Phone/Views/AgregarPedido.xaml.cs
+++ Cueros.App.Phone/Views/AgregarPedido.xaml.cs
@@ -15,6 +15,26 @@ namespace Cueros.App.Phone.Views
         public AgregarPedido()
         {
             InitializeComponent();
+            Loaded += AgregarPedido_Loaded;
+        }
+
+        void AgregarPedido_Loaded(object sender, RoutedEventArgs e)
+        {
+            CargarAppBar();
+        }
+        private void CargarAppBar()
+        {
+            ApplicationBar = new ApplicationBar();
+            ApplicationBarIconButton GuardarPedido = new ApplicationBarIconButton(new Uri("/Assets/AppBar/refresh.png", UriKind.Relative));
+            GuardarPedido.Text = "Guardar Pedido";
+            ApplicationBar.Buttons.Add(GuardarPedido);
+            GuardarPedido.Click += GuardarPedido_Click;
+        }
+
+        void GuardarPedido_Click(object sender, EventArgs e)
+        {
+            //mandar a la lista de pedidos
+            NavigationService.GoBack();
         }
     }
 }
